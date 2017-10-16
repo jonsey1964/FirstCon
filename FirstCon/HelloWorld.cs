5a16d0d0487e62dd091b78e27af3c50015623234
Owen Jones <omj@jonsie.freeserve.co.uk>
2017-10-16 14:03:52 +0000

    added new class

---
+++ FirstCon/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCon
+{
+    class HelloWorld
+    {
+        private string HelloMsg;
+
+        public string Hello
+        {
+            get { return HelloMsg; }
+            set { HelloMsg = value; }
+        }
+    }
+        
+    
+}
